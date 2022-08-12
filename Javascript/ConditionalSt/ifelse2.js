527db45d85edc136c1e79dc3daa7c02a4dd0c40f
Mohd-Imad <mohdimd123@gmail.com>
2022-08-12 20:43:31 +0530

    JS Practice

---
+++ Javascript/ConditionalSt/ifelse2.js
@@ -0,0 +1,8 @@
+//WAP to check whether the given number is even or odd
+
+var num=2;
+if (num % 2 ==0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
---
+++ Javascript/JS-Assgnmnts/as1.js
@@ -0,0 +1,20 @@
+/* Divisible */
+
+var num=84;
+
+if (num % 7==0) {
+console.log('Yes, it is divisible by 7');    
+} 
+else {
+   console.log('No, it is not divisible by 7'); 
+}
+
+/* Not Divisible */
+
+var num=120
+if (num % 7==0) {
+    console.log('Yes, it is divisible by 7');    
+    } 
+    else {
+       console.log('No, it is not divisible by 7'); 
+    }
