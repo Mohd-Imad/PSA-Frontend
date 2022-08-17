3641fc2ba11b91bdad40e56ff519f926ef7bc8ba
Mohd-Imad <mohdimd123@gmail.com>
2022-08-17 22:12:45 +0530

    JS-Array & Object

---
+++ Javascript/Array/arr1.js
@@ -0,0 +1,19 @@
+
+//Creating an array
+var a = []  //empty array
+var b = [10, 20, 30, 40]  //
+var c = [10, 10, true, undefined, 'rahul', [], {}]
+//array allowed duplicates and heterogenuous elements
+
+
+//Reading an array
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(c[4]);
+
+
+//Updating an array
+c[4]= 'Rahul Gandhi';
+
+console.log(c[4]);
