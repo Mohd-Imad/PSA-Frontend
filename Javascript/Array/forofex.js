4e1ad0348fb3449b430333c24af7bacfa294d74e
Mohd-Imad <mohdimd123@gmail.com>
2022-08-22 17:19:35 +0530

    JS cont

--- Javascript/Array/forofex.js
+++ Javascript/Array/forofex.js
@@ -1,5 +1,5 @@
 let a = [100, 200, 300]
 console.log(a)
-for (values of a) {
+for (values of a) {          //values-----temporary variable
     console.log(values)
 }
---
+++ Javascript/JS-Assgnmnts/Events/fun1.js
@@ -0,0 +1,25 @@
+function changeColor1() {
+    document.getElementById('mover').style.backgroundColor='red'
+}
+
+function changeColor2() {
+    document.getElementById('mout').style.backgroundColor="yellow"
+}
+
+function changeColor3() {
+    document.getElementById('onclk').style.backgroundColor="green"
+}
+
+function changeColor4() {
+    document.getElementById('ondblclk').style.backgroundColor="blue"
+}
+
+
+function changeBg() {
+    document.getElementById('onfoc').style.backgroundColor="aqua"
+}
+
+function lowToUp() {
+    var x = document.getElementById('onblr')
+    x.value = x.value.toUpperCase();
+}
